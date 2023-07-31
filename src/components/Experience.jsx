9d1d45759a623852e3edea33d1d68429f98f2c5c
nehadawad <nehadakash622@gmail.com>
2023-07-31 12:14:59 +0600

    mesh portal metarial effect added on fish model

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,20 +1,34 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import {
+  Environment,
+  MeshPortalMaterial,
+  OrbitControls,
+  RoundedBox,
+  useTexture,
+} from "@react-three/drei";
 import * as THREE from "three";
+import { Fish } from "./Fish";
 
 export const Experience = () => {
-
   const map = useTexture(
     "textures/anime_art_style_a_water_based_pokemon_like_environ.jpg"
-    )
+  );
   return (
     <>
-      <ambientLight intensity={0.5}/>
-        <Environment preset="sunset" />
+      <ambientLight intensity={0.5} />
+      <Environment preset="sunset" />
       <OrbitControls />
-      <mesh>
-        <sphereGeometry args={[5, 64, 64]} />
-        <meshStandardMaterial map={map} side={THREE.BackSide} />
-      </mesh>
+
+      <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          <Fish scale={0.4} position-y={-0.7} />
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
     </>
   );
 };
